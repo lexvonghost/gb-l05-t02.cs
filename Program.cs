a0f5bd37bcb3260a185ca0315b52bf65610e3d2d
lexvonghost <lex_von_tost@mail.ru>
2022-08-21 20:59:39 +0300

    feat: создание массива

--- Program.cs
+++ Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Задача 36: Задайте одномерный массив, заполненный случайными числами.
+// Найдите сумму элементов, стоящих на нечётных позициях.
+
+Console.WriteLine("Введите число, определяющее длинну массива");
+int length =  Convert.ToInt32(Console.ReadLine());
+int[] NewArray(int[] array)
+{
+    for (int i = 0; i < length; i++)
+    {
+        array[i] = new Random().Next(100, 1000);
+    }
+
+    return array;
+}
+int[] array = new int[length];
+array = NewArray(array);
+Console.WriteLine("[" + string.Join(", ", array) + "]");
